140bcd4dc6c61cd303b84da8f1dc4cc855edb9bf
Hwang Seong Taek <79950091+6uamy@users.noreply.github.com>
2022-05-23 21:27:48 +0900

    Update Rank_test.sol

--- Rank_test.sol
+++ Rank_test.sol
@@ -34,14 +34,14 @@ contract P2DToken is ERC20 {
         for(uint i=0; i<topScores.length-1; i++){
             for(uint j=0; j<topScores.length-1-i; j++){
                 if(topScores[j].score < topScores[j+1].score){
-                    uint currentScore = topScores[j].score;
-                    topScores[j].score = topScores[j+1].score;
-                    topScores[j+1].score = currentScore;
+                   // uint currentScore = topScores[j].score;
+                    TopScore memory currentScore = topScores[j];
+                    topScores[j] = topScores[j+1];
+                    topScores[j+1]= currentScore;
                 }
             }
         }
     }
-
     // 배열의 특정 값과 for 문을 이용하여 배열의 전체 값을 불러 오기 위해 _number 라는 파라미터를 입력 받는다.
     function getUserScore(uint _number) public view returns(address user_addr, uint user_score){
         user_addr = topScores[_number].addr;
