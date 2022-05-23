3eb06b272ab60483a8616d8765c28fb4452378af
Hwang Seong Taek <79950091+6uamy@users.noreply.github.com>
2022-05-23 20:35:10 +0900

    Update Rank_test.sol

--- Rank_test.sol
+++ Rank_test.sol
@@ -31,56 +31,26 @@ contract P2DToken is ERC20 {
         TopScore memory topScore = TopScore(msg.sender, score);
         topScores.push(topScore);
 
-        // uint256 currentTopScore = _userscore[msg.sender];
-        // if(currentTopScore < score){
-        //     _userscore[msg.sender] = score;
-        // }
-
-        // if(topScores.length < maxTopScores){
-        //     topScores.push(TopScore(msg.sender, score));
-
-            for(uint i=0; i<topScores.length-1; i++){
-                for(uint j=0; j<topScores.length-1-i; j++){
-                    if(topScores[j].score < topScores[j+1].score){
-                        uint currentScore = topScores[j].score;
-                        topScores[j].score = topScores[j+1].score;
-                        topScores[j+1].score = currentScore;
-                    }
+        for(uint i=0; i<topScores.length-1; i++){
+            for(uint j=0; j<topScores.length-1-i; j++){
+                if(topScores[j].score < topScores[j+1].score){
+                    uint currentScore = topScores[j].score;
+                    topScores[j].score = topScores[j+1].score;
+                    topScores[j+1].score = currentScore;
                 }
             }
-        //배열이 가득안찼으면 push 하는것까지는 ok .-> push하고 바로 순위 비교해서 sort 해줘야지 
-        //     }else{
-        //     uint256 lowestScore = 0;
-        //     uint256 lowestScoreIndex = 0;
-        //     for (uint256 i = 0; i < topScores.length; i++)
-        //     {
-        //         TopScore storage currentScore = topScores[i];
-        //         if(i == 0){
-        //             lowestScore = currentScore.score;
-        //             lowestScoreIndex = i;
-        //         }else{
-        //             if(lowestScore > currentScore.score){
-        //                 lowestScore = currentScore.score;
-        //                 lowestScoreIndex = i;
-        //             }
-        //         }
-        //     }
-        // }
-
-
+        }
     }
 
-// 배열의 특정 값과 for 문을 이용하여 배열의 전체 값을 불러 오기 위해 _number 라는 파라미터를 입력 받는다.
+    // 배열의 특정 값과 for 문을 이용하여 배열의 전체 값을 불러 오기 위해 _number 라는 파라미터를 입력 받는다.
     function getUserScore(uint _number) public view returns(address user_addr, uint user_score){
         user_addr = topScores[_number].addr;
         user_score = topScores[_number].score;
     }
-
-
-        // token transfer
-        function send(uint256 _value) payable public {
-        require(_value >= amount);
-        require(_value < _balances[msg.sender]);
-        ERC20.transfer(_addr, _value);
+    // token transfer
+    function send(uint256 _value) payable public {
+    require(_value >= amount);
+    require(_value < _balances[msg.sender]);
+    ERC20.transfer(_addr, _value);
     }
 }
